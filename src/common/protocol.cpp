295ae3fd7bce952ea62d76e73d7dd7b42b06b3ce
hyird <hyird@outlook.com>
2026-01-10 11:21:05 +0800

    'x'

--- src/common/protocol.cpp
+++
@@ -1,8 +0,0 @@
-#include "common/protocol.hpp"
-
-namespace edgelink {
-
-// Protocol constants and types are all defined in the header
-// This file is for any non-trivial implementations
-
-} // namespace edgelink
